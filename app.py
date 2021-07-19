f3dfde6a82c334474dd5f891751f59947206da19
Katherine <kat2chao@gmail.com>
2021-07-19 18:03:18 +0000

    giphy dog

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 # -- Import section --
 from flask import Flask, render_template, request
 from datetime import datetime
-# from model import getImageUrlFrom
+from model import getImageUrlFrom
 import os
 
 # -- Initialization section --
@@ -14,3 +14,18 @@ app = Flask(__name__)
 @app.route('/index')
 def index():
     return render_template("index.html", time = datetime.now())
+
+# add route for your gif results
+@app.route('/yourgif', methods = ["GET", "POST"])
+def yourgif():
+    user_response = "dog"
+    gifLink = getImageUrlFrom(user_response)
+    print(gifLink)
+    
+    #pass URL to render template and have that URL appear as an image in yourgif.html
+
+    # get the gif for giphy and puts the link on the webpage
+    return render_template("yourgif.html", time = datetime.now(), gifLink = gifLink)
+    # add datetime.now() to trick our browser into updating the css if we make any changes
+
+    
--- model.py
+++ model.py
@@ -1,2 +1,8 @@
+import requests
+
+
 def getImageUrlFrom(query):
-    pass
+    giphy_request_link = "https://api.giphy.com/v1/gifs/search?api_key=7BbMzq8ZT7aldPWWINWFUCtlZ1a7gVZq&q=dog&limit=25&offset=0&rating=g&lang=en"
+    response = requests.get(giphy_request_link).json()
+    return response["data"][0]["images"]["fixed_height"]["url"]
+
